05b0ae4423d1702eaffb0f594819cd16216d35d4
zyl-GitHub <zyldomain@163.com>
2019-05-17 14:54:48 +0800

    修改Test

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,23 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Man man = new Child();
+        System.out.println(man.age);
+        man.say();
     }
+}
+
+class Man {
+    int age = 0;
+    public void say(){
+        System.out.println("man");
+    }
+}
+
+class Child extends  Man{
+    int age = 1;
+
+    public void say(){
+        System.out.println("child");
+    }
+
 }
